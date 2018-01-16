ac6485e5430d3ab464699355fa8e0b827ae05fdb
Maximiliano Opitz Turra <Maximiliano@MacBook-Pro-de-Maximiliano.local>
2018-01-16 19:30:25 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ToastAndroid
+} from 'react-native';
+
+var PushNotification = require('react-native-push-notification');
+
+PushNotification.configure({
+
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: function(token) {
+
+    },
+
+    onNotification: function(notification) {
+      setTimeout(() => {
+        if(!notification['foreground']){
+          console.warn(notification)
+        }
+      }, 1);
+      PushNotification.localNotificationSchedule({
+        title: 'Walter is doing tradding',
+        message: notification['nombre'], // (required)
+        date: new Date(Date.now()) // in 60 secs
+      });
+    },
+
+    // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications)
+    senderID: "1045527139327",
+
+});
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
